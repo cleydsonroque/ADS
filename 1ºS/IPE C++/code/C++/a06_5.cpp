89bceeedf9e62c32092795ccd8b1500781d0d49e
Cleydson Roque S. R <croque10@yahoo.com.br>
2020-05-29 14:28:12 -0300

    Update a06_5.cpp

--- 1ºS/IPE C++/code/C++/a06_5.cpp
+++ 1ºS/IPE C++/code/C++/a06_5.cpp
@@ -3,26 +3,28 @@
 using namespace std;
 
 int main (){
-	setlocale(LC_ALL, "pt-BR");
+	setlocale(LC_ALL, "Portuguese");
 	int ped, qtde;
 	float val;
 	bool inv;
 	
-	cout << "\n       *** Card�pio *** \n";
-	cout << "\n   100 � Hamb�rguer � R$5,50 \n";
-	cout << "\n   101 � Cachorro-quente � R$4,50 \n";
-	cout << "\n   102 � Milkshake � R$7,00 \n";
-	cout << "\n   103 � Pizza brotinho � R$8,00 \n";
-	cout << "\n   104 - Cheeseburguer � R$8,50 \n";
-	cout << "\n   Informe o c�digo do seu pedido: \n";
+	cout << "\n       *** Cardápio *** \n";
+	cout << "\n   100 – Hambúrguer – R$5,50 \n";
+	cout << "\n   101 – Cachorro-quente – R$4,50 \n";
+	cout << "\n   102 – Milkshake – R$7,00 \n";
+	cout << "\n   103 – Pizza brotinho – R$8,00 \n";
+	cout << "\n   104 - Cheeseburguer – R$8,50 \n";
+	cout << "\n   Informe o código do seu pedido: \n";
 	cin >> ped;
 	
 		if (ped != 100 || ped != 101 || ped != 102 || ped != 103 || ped != 104 ) {
-			cout << "\n    C�digo inv�lido, informe o c�digo do seu pedido: \n";
+			cout << "\n    Código inválido, informe o código do seu pedido: \n";
 			cin >> ped;
 		}
 	
-	//switch (ped) {
+	//switch (ped) {ucódigo
+
+
 	//	case 100: 
 	//}
 	
