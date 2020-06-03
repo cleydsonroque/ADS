88787b09b4172ba00a8272ee24c2b77e00ef73aa
Cleydson Roque S. R <croque10@yahoo.com.br>
2020-06-03 10:07:59 -0300

    Modified file

--- 1ºS/IPE C++/code/C++/teste.cpp
+++ 1ºS/IPE C++/code/C++/teste.cpp
@@ -5,22 +5,14 @@ using namespace std;
 
 int main () {
 	setlocale(LC_ALL, "Portuguese");
-	int aux,lin,col,M1[2][2],M2[2][2];
-	for (lin=0;lin<=1;lin++) {
-		for (col=0;col<=1;col++) {
-			cout <<"Informe o elemento ["<<lin+1<<","<<col+1<<"] da Matriz 1: ";
-			cin >>M1[lin][col];
-			cout <<"Informe o elemento ["<<lin+1<<","<<col+1<<"] da Matriz 2: ";
-			cin >>M2[lin][col];
-			if (lin==col){
-				aux=M1[lin][col];
-				M1[lin][col]=M2[lin][col];
-				M2[lin][col]=aux;
-			}
-		}
-	}
-	for (lin=0;lin<=1;lin++) {
-		cout <<"Elemento ["<<lin+1<<","<<lin+1<<"] da Matriz 1: "<<M1[lin][lin]<<"\n";
-		cout <<"Elemento ["<<lin+1<<","<<lin+1<<"] da Matriz 2: "<<M2[lin][lin]<<"\n";
+	int z;
+	cout << "Digite um valor : ";
+	cin >> z;
+	switch(z) {
+		case 5 : cout << 3 * z << " ";
+		case 10 : cout << 11 /2 * z << " ";
+		case 20 : cout << z * z - 10 << endl;
+		break;
+		default : cout << "Entrada invalida. " << endl;
 	}
 }
